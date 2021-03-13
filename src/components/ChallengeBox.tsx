a984f1e1f9cdd0ddf15ee55e1358f51336e32ca6
Valdeir Silva <valdeir.jobs@gmail.com>
2021-03-13 16:17:35 -0300

    ajustando botão falhei

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -14,6 +14,7 @@ export function ChallengeBox() {
 
     function handleChallengeFailed() {
         resetChallenge();
+        resetCountdown();
     }
     return (
         <div className={styles.challengeBoxContainer}>
