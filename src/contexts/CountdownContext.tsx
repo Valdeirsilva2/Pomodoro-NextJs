43449b237fe530adb0b7cc1a24c7c0d56e78b0fe
Valdeir Silva <valdeir.jobs@gmail.com>
2021-03-13 23:48:49 -0300

    ajustando time do countdown

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -35,7 +35,7 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
     function resetCountdown() {
         setIsActive(false);
         clearTimeout(countdownTimeout);
-        setTime(0.1 * 60);
+        setTime(25 * 60);
         setHasFinished(false);
     }
 
